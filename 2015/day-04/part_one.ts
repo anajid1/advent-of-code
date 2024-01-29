bef6a89e0fcf4eda6b9adcd0b6c46061b46fbb22
Abdullah Najid <90467466+anajid1@users.noreply.github.com>
2024-01-28 22:23:39 -0500

    Complete part 1 and 2 for day 4

--- 2015/day-04/part_one.ts
+++ 2015/day-04/part_one.ts
@@ -1,3 +1,5 @@
+// INCLUDES PART 2
+
 import {Md5} from 'ts-md5';
 
 import * as fs from 'fs';
@@ -6,7 +8,7 @@ let key = fs.readFileSync('input.txt', 'utf8');
 
 let answer: string = "0";
 
-while (Md5.hashStr(key+answer).substring(0,5) != "00000") {
+while (Md5.hashStr(key+answer).substring(0,6) != "000000") {
     let temp = +answer + 1;
     answer = temp+"";
 }
